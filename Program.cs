96b146863e70631f36810d2d8d48ac5c2bb1ef4c
ferbarcante <barcantef@gmail.com>
2025-02-21 14:39:11 -0300

    criando menu

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data;
+using System.Runtime.InteropServices;
 using Microsoft.Data.Sqlite;
+using Microsoft.VisualBasic.FileIO;
 
 
 namespace HabitTrackerApp 
@@ -27,5 +30,70 @@ namespace HabitTrackerApp
                 connection.Close();
             }
         }
+
+        static void GetUserInput()
+        {
+            Console.Clear();
+            bool closeApp = false;
+            
+            while (closeApp = false)
+            {
+                Console.WriteLine("\n\nMAIN MENU");
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("Type 1 to view all records");
+                Console.WriteLine("Type 2 to add a new record");
+                Console.WriteLine("Type 3 to delete a record");
+                Console.WriteLine("Type 4 to update a record");
+                Console.WriteLine("------------------------------------------------\n");
+
+                string commandInput = Console.ReadLine();
+
+                switch(commandInput)
+                {
+                    case 1:
+                    // View all records
+                        GetAll();
+                        closeApp = true;
+                        break;
+                    case 2:
+                        Create();
+                        closeApp = true;
+                        break;
+                    case 3:
+                        Delete();
+                        closeApp = true;
+                        break;
+                    case 4:
+                        Update();
+                        closeApp = true;
+                        break; 
+                    default:
+                        Console.WriteLine("Invalid input. Please try again.");
+                        break;               
+                }
+            }
+        }
+
+        static void GetAll()
+        {
+
+        }
+
+        static void Create()
+        {
+            
+        }
+
+        static void Delete()
+        {
+
+        }
+
+        static void Update()
+        {
+
+        }
     }
+
+    
 }
