c1e8a738b5be0ad4f74b67be21616c82dbfba30d
ferbarcante <barcantef@gmail.com>
2025-02-21 14:28:08 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+
+namespace HabitTrackerApp 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=HabitTrackerApp.db";
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = 
+                    @"CREATE TABLE IF NOT EXISTS drinking_water(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        );";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+    }
+}
